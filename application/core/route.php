0a123900d0332b3f1bbef73d24f17b1e7020f073
DarkNogard <nogard_5@mail.ru>
2020-09-07 17:18:45 +0300

    commit 1

--- application/core/route.php
+++ application/core/route.php
@@ -1,10 +1,6 @@
 <?php
 
-/*
-Класс-маршрутизатор для определения запрашиваемой страницы.
-> цепляет классы контроллеров и моделей;
-> создает экземпляры контролеров страниц и вызывает действия этих контроллеров.
-*/
+
 class Route
 {
 
@@ -33,13 +29,7 @@ class Route
 		$controller_name = 'Controller_'.$controller_name;
 		$action_name = 'action_'.$action_name;
 
-
-//		echo "Model: $model_name <br>";
-//		echo "Controller: $controller_name <br>";
-//		echo "Action: $action_name <br>";
-
-
-		// подцепляем файл с классом модели (файла модели может и не быть)
+		// подцепляем файл с классом модели
 
 		$model_file = strtolower($model_name).'.php';
 		$model_path = "application/models/".$model_file;
@@ -57,10 +47,7 @@ class Route
 		}
 		else
 		{
-			/*
-			правильно было бы кинуть здесь исключение,
-			но для упрощения сразу сделаем редирект на страницу 404
-			*/
+
 			Route::ErrorPage404();
 		}
 		
@@ -75,7 +62,7 @@ class Route
 		}
 		else
 		{
-			// здесь также разумнее было бы кинуть исключение
+
 			Route::ErrorPage404();
 		}
 	
